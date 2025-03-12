3fbeb6d66033e85b67a09a2b90fe18d785e9eeb3
Kachi <onyedikannaji7@gmail.com>
2025-03-12 07:17:39 +0100

    new

--- index.js
+++ index.js
@@ -1,60 +1,27 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const faqItems = document.querySelectorAll('.faq-item');
-
-//     faqItems.forEach(item => {
-//         const question = item.querySelector('.faq-question');
-//         const answer = item.querySelector('.faq-answer');
-
-//         question.addEventListener('click', () => {
-//             toggleAnswer(item, answer);
-//         });
-
-//         question.addEventListener('keydown', (e) => {
-//             if (e.key === 'Enter' || e.key === ' ') {
-//                 e.preventDefault();
-//                 toggleAnswer(item, answer);
-//             }
-//         });
-//     });
-
-//     function toggleAnswer(item, answer) {
-//         const isOpen = item.classList.contains('open');
-//         faqItems.forEach(i => i.classList.remove('open'));
-//         if (!isOpen) {
-//             item.classList.add('open');
-//         }
-//     }
-// });
 document.addEventListener('DOMContentLoaded', () => {
     const faqItems = document.querySelectorAll('.faq-item');
-    const iconPlusContainers = document.querySelectorAll('.icon-plus-container');
 
     faqItems.forEach(item => {
         const question = item.querySelector('.faq-question');
         const answer = item.querySelector('.faq-answer');
 
         question.addEventListener('click', () => {
-            toggleAnswer(item, answer);
+            toggleAnswer(item);
         });
 
         question.addEventListener('keydown', (e) => {
             if (e.key === 'Enter' || e.key === ' ') {
                 e.preventDefault();
-                toggleAnswer(item, answer);
+                toggleAnswer(item);
             }
         });
     });
 
-    iconPlusContainers.forEach(container => {
-        container.addEventListener('click', () => {
-            container.querySelector('p').classList.toggle('active');
-        });
-    });
-
-    function toggleAnswer(item, answer) {
+    function toggleAnswer(item) {
         const isOpen = item.classList.contains('open');
-        faqItems.forEach(i => i.classList.remove('open'));
-        if (!isOpen) {
+        if (isOpen) {
+            item.classList.remove('open');
+        } else {
             item.classList.add('open');
         }
     }
